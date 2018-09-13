3dbcd1732c357dfbb0637ea25424161997dbddfd
Nox-0 <dbublik11@gmail.com>
2018-09-13 20:11:47 +1200

    Updated level design ideas
    I have added colours to the keys, adjusted the enemies per level, added a theme to each level and added a todo list

--- game.py
+++ game.py
@@ -1,20 +1,33 @@
 """
 Level 1:
-10 Snakes
-1 key
+1 Snake
+Green key
+Grassy
+Is essentially a tutorial
 
 Level 2:
-15 Snakes
-1 key
+5 Snakes
+Orange key
+Dirty
 
 Level 3:
-20 Snakes
-1 key
+10 Snakes
+Yellow key
+Sandy
 
 Level 4:
-30 Snakes
-1 key
+20 Snakes
+Blue key
+Snowy
 
 Level 5:
 Medusa
+Stony
+
+TODO: Make the levels, make the character, make the enemies, give each health, implement keys
+Level 1: In development
+Level 2: Not started
+Level 3: Not started
+Level 4: Not started
+Level 5: Not started
 """
